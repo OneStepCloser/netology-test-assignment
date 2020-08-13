389d172285421e8d30dcd5e49d4144e50d629722
OneStepCloser <artinspira@gmail.com>
2020-08-13 23:01:25 +0300

    Add data mock

---
+++ src/assets/js/mocks.js
@@ -0,0 +1,56 @@
+const employees = [
+  {
+    id: 1,
+    firstName: 'Иван',
+    lastName: 'Кудрин',
+    age: 20,
+  },
+  {
+    id: 2,
+    firstName: 'Владислав',
+    lastName: 'Веткин',
+    age: 20,
+  },
+  {
+    id: 3,
+    firstName: 'Сергей',
+    lastName: 'Захарченко',
+    age: 22,
+  },
+  {
+    id: 4,
+    firstName: 'Даниил',
+    lastName: 'Трофинин',
+    age: 19,
+  },
+  {
+    id: 5,
+    firstName: 'Глеб',
+    lastName: 'Черненко',
+    age: 25,
+  },
+  {
+    id: 6,
+    firstName: 'Ислам',
+    lastName: 'Утюжев',
+    age: 23,
+  },
+  {
+    id: 7,
+    firstName: 'Алёна',
+    lastName: 'Полянская',
+    age: 24,
+  },
+  {
+    id: 8,
+    firstName: 'Даниил',
+    lastName: 'Глущинский',
+    age: 20,
+  },
+  {
+    id: 9,
+    firstName: 'Ринат',
+    lastName: 'Кишников',
+    age: 22,
+  },
+];
