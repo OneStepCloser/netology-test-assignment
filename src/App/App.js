90b4ec8180d04ca160af41d57d9aa80014e59723
OneStepCloser <artinspira@gmail.com>
2020-08-13 22:07:12 +0300

    Add header component and content wrapper

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import styles from './App.module.scss'
+import AppHeader from 'components/AppHeader/AppHeader';
 
 function App() {
   return (
     <div className={styles['app-root']}>
-      Hello world
+      <AppHeader/>
+      <div className={styles['content']}>
+        <div className={styles['content-card']}>
+          Hello world
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/AppHeader/AppHeader.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './AppHeader.module.scss'
+
+export default class AppHeader extends React.Component {
+  render() {
+    return (
+      <div className={styles['app-header-root']}>
+        <img
+          src={ require('assets/img/users.svg') }
+          className={ styles['icon'] }
+        />
+        <h1 className={ styles['header'] }>Список сотрудников</h1>
+      </div>
+    )
+  }
+}
