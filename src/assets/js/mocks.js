fb9a520fd29f07db1107ce0cb7c75f04c0df0418
OneStepCloser <artinspira@gmail.com>
2020-08-14 18:39:41 +0300

    Replace mock data with mock api calling function

--- src/assets/js/mocks.js
+++ src/assets/js/mocks.js
@@ -1,60 +1,9 @@
-const employees = [
-  {
-    id: 1,
-    firstName: 'Иван',
-    lastName: 'Кудрин',
-    age: 20,
-  },
-  {
-    id: 2,
-    firstName: 'Владислав',
-    lastName: 'Веткин',
-    age: 20,
-  },
-  {
-    id: 3,
-    firstName: 'Сергей',
-    lastName: 'Захарченко',
-    age: 22,
-  },
-  {
-    id: 4,
-    firstName: 'Даниил',
-    lastName: 'Трофинин',
-    age: 19,
-  },
-  {
-    id: 5,
-    firstName: 'Глеб',
-    lastName: 'Черненко',
-    age: 25,
-  },
-  {
-    id: 6,
-    firstName: 'Ислам',
-    lastName: 'Утюжев',
-    age: 23,
-  },
-  {
-    id: 7,
-    firstName: 'Алёна',
-    lastName: 'Полянская',
-    age: 24,
-  },
-  {
-    id: 8,
-    firstName: 'Даниил',
-    lastName: 'Глущинский',
-    age: 20,
-  },
-  {
-    id: 9,
-    firstName: 'Ринат',
-    lastName: 'Кишников',
-    age: 22,
-  },
-];
+export async function loadEmployees() {
+  const resp = await fetch('https://5f35e9bb5b91f60016ca519d.mockapi.io/employees');
 
-export {
-  employees,
-};
+  if (resp.ok) {
+    return await resp.json();
+  } else {
+    throw new Error('Произошла ошибка при загрузке данных');
+  }
+}
--- src/containers/EmployeesTable/EmployeesTable.js
+++ src/containers/EmployeesTable/EmployeesTable.js
@@ -4,7 +4,6 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import { employees } from 'assets/js/mocks';
 
 const columns = [
   {
@@ -23,23 +22,25 @@ const columns = [
 
 export default class EmployeesTable extends React.Component {
   render() {
-    return (
-      <Table>
-        <TableHead>
-          <TableRow>
-            { columns.map(col => <TableCell key={col.key}>{ col.title }</TableCell>) }
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          { employees.map((emp) => (
-            <TableRow key={ emp.id }>
-              <TableCell>{ emp.firstName }</TableCell>
-              <TableCell>{ emp.lastName }</TableCell>
-              <TableCell>{ emp.age }</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    );
+    return <div></div>
+
+    // return (
+    //   {/*<Table>
+    //     <TableHead>
+    //       <TableRow>
+    //         { columns.map(col => <TableCell key={col.key}>{ col.title }</TableCell>) }
+    //       </TableRow>
+    //     </TableHead>
+    //     <TableBody>
+    //       { employees.map((emp) => (
+    //         <TableRow key={ emp.id }>
+    //           <TableCell>{ emp.firstName }</TableCell>
+    //           <TableCell>{ emp.lastName }</TableCell>
+    //           <TableCell>{ emp.age }</TableCell>
+    //         </TableRow>
+    //       ))}
+    //     </TableBody>
+    //   </Table>*/}
+    // );
   }
 };
